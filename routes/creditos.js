d4afe3b68aef3d6e681f95288e571477eaec458d
Fernando <dferramm@gmail.com>
2025-12-25 01:23:21 -0600

    ChrIsTmAs

--- routes/creditos.js
+++ routes/creditos.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { authenticate } = require("../middlewares/authMiddleware");
-const { authorizeAdmin } = require("../middlewares/checkCreditAccess");
+const { authenticate, authorizeAdmin } = require("../middlewares/authMiddleware");
 const creditoController = require("../controllers/creditoController");
 
 // Rutas protegidas para administradores
